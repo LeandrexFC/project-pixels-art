8b5ba326377c57416d561833f0dc5e5b746bfe07
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-08-31 19:58:40 -0300

    Requisito 1 e 2 concluídos

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    let change = document.querySelector(".color");
+    change.style.backgroundColor = "blue"
+
+    let change2 = document.querySelector(".color").nextElementSibling;
+    change2.style.backgroundColor = "red"
+
+    let change3 = change2.nextElementSibling;
+    change3.style.backgroundColor = "yellow"
+
+    let change4 = change3.nextElementSibling;
+    change4.style.backgroundColor = "green"
+}
