e2a21e07b2fcfb7bdcbea76056c2ccb963fec04d
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-09-01 17:20:46 -0300

    Requisito 4 concluído

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ window.onload = function () {
     change.style.backgroundColor = "black"
 
     let change2 = document.querySelector(".color").nextElementSibling;
-    change2.style.backgroundColor = "blue"
 
     let change3 = change2.nextElementSibling;
-    change3.style.backgroundColor = "red"
 
     let change4 = change3.nextElementSibling;
-    change4.style.backgroundColor = "yellow"
+
+    let btn = document.querySelector("#button-random-color")
+    btn.addEventListener('click', function () {
+        let r = parseInt(Math.random() * 255);
+        let g = parseInt(Math.random() * 255);
+        let b = parseInt(Math.random() * 255);
+        change2.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+        change3.style.backgroundColor = 'rgb(' + b + ',' + r + ',' + g + ')';
+        change4.style.backgroundColor = 'rgb(' + g + ',' + b + ',' + r + ')';
+
+    })
 }
